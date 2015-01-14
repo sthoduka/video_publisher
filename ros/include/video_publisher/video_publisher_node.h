5cc6aba61a43bc953c9002bc45a832183e2129a1
Santosh Thoduka <santosh.thoduka@gmail.com>
2015-01-14 15:52:28 +0100

    add copyright notice

--- ros/include/video_publisher/video_publisher_node.h
+++ ros/include/video_publisher/video_publisher_node.h
@@ -1,3 +1,11 @@
+/* video_publisher_node.h
+ *
+ * Copyright (C) 2014 Santosh Thoduka
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #ifndef VIDEO_PUBLISHER_NODE_H_
 #define VIDEO_PUBLISHER_NODE_H_
 
--- ros/src/video_publisher_node.cpp
+++ ros/src/video_publisher_node.cpp
@@ -1,3 +1,11 @@
+/* video_publisher_node.cpp
+ *
+ * Copyright (C) 2014 Santosh Thoduka
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #include <video_publisher/video_publisher_node.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/opencv.hpp>
